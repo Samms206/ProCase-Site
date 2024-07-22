26858b0f5fbd75b955b6831ac884e47b97c948af
Samms206 <sh2069936@gmail.com>
2024-07-22 21:03:27 +0700

    first commit

---
+++ procase-app/src/components/Phone.tsx
@@ -0,0 +1,34 @@
+import { cn } from "@/lib/utils";
+import React, { HtmlHTMLAttributes } from "react";
+
+interface PhoneProps extends HtmlHTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+  dark?: boolean;
+}
+
+const Phone = ({ className, imgSrc, dark = false, ...props }: PhoneProps) => {
+  return (
+    <div
+      className={cn(
+        "relative pointer-events-none z-50 overflow-hidden",
+        className
+      )}
+      {...props}
+    >
+      <img
+        className="pointer-events-none z-50 select-none"
+        src={
+          dark
+            ? "/phone-template-dark-edges.png"
+            : "/phone-template-white-edges.png"
+        }
+        alt="phoneiimages.png"
+      />
+      <div className="absolute -z-10 inset-0">
+        <img className="object-cover" src={imgSrc} alt="overlaying.png" />
+      </div>
+    </div>
+  );
+};
+
+export default Phone;
